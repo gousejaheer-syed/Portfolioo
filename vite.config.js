36149ee668fba957e7ffa019d3e43171fcb8c695
Syed Gouse Jaheer <sgousejaheer@gmail.com>
2026-07-11 14:11:20 +0530

    fix: update base path,

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolioo/', // Change 'Portfolioo' to your exact repo name
+  // This tells the browser to look for files in /Portfolioo/
+  base: '/Portfolioo/',
 })
